f3bf1116a5f87532f2357ffe297a68ebd4cee97b
Илья Шульц <anarim2017@mail.ru>
2021-08-05 05:28:42 +0700

    Поправки для меню

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ TOKEN = "1538347584:AAHNLECRF2peOGDaeMAyAAMHORTD_EyPbGk"
 PAYMENTS_PROVIDER_TOKEN = '381764678:TEST:23779'
 TIME_MACHINE_IMAGE_URL = 'https://img.desktopwallpapers.ru/animals/pics/wide/1920x1200' \
                          '/c24f98eba82666ae547b138be9933bd1.jpg '
+pass
